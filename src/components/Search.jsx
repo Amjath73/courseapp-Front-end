56a89868e5c2d6ac457c64a8f3de347c17f7223a
Amjath73 <amjathsanker4@gmail.com>
2024-06-12 14:34:02 +0530

    Search and delete completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,10 +10,27 @@ const Search = () => {
 
     const [result, setResult] = useState([])
 
+    // delete and event handling
+    const deleteCourse = (id) => {
+        let input = {"_id": id}
+        axios.post("http://localhost:8081/delete",input).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("Successfully deleted")
+                } else {
+                    alert("error in deletion")
+                }
+            }
+        ).catch().finally()
+    }
 
+    // value fetching
     const inputHandler = (event) => {
         setData({ ...data, [event.target.name]: event.target.value })
     }
+
+    // search button
     const readValue = () => {
         console.log(data)
         axios.post("http://localhost:8081/search", data).then(
@@ -54,29 +71,29 @@ const Search = () => {
                                         </thead>
                                         <tbody>
                                             {
-                                            result.map(
-                                                    (value,index)=>{
-                                                        return  <tr>
-                                                        <th>{value.courseTitle}</th>
-                                                        <td>{value.description}</td>
-                                                        <td>{value.date}</td>
-                                                        <td>{value.duration}</td>
-                                                        <td>{value.venue}</td>
-                                                        <td>{value.trainerName}</td>
-                                                        <td><button className="btn btn-danger">DELETE</button></td>
-                                                    </tr>
-                                                }
+                                                result.map(
+                                                    (value, index) => {
+                                                        return <tr>
+                                                            <th>{value.courseTitle}</th>
+                                                            <td>{value.description}</td>
+                                                            <td>{value.date}</td>
+                                                            <td>{value.duration}</td>
+                                                            <td>{value.venue}</td>
+                                                            <td>{value.trainerName}</td>
+                                                            <td><button className="btn btn-danger" onClick={()=>{deleteCourse(value._id)}}>DELETE</button></td>
+                                                        </tr>
+                                                    }
                                                 )
                                             }
                                         </tbody>
                                     </table>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
